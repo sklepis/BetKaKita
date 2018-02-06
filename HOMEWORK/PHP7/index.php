c4456df0a3f4455bfa04ec5a23efa5c8faa688f3
sklepis <panda.spaikis@gmail.com>
2018-02-06 19:50:19 +0200

    +1 PHP

--- HOMEWORK/PHP7/index.php
+++ HOMEWORK/PHP7/index.php
@@ -3,7 +3,8 @@
 
     getAge($yearOfBirth);
 
-    function getAge($yearOfBirth) {
+    function getAge($yearOfBirth)
+    {
         echo
         "<h3> Jūsų gimimo metai yra ".$yearOfBirth.
         ". Jums dabar arba šiais metais bus ".(date("Y") - $yearOfBirth).".</h3>";
